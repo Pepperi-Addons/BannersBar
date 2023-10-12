292be140780e583af3f52e1d0c91029a7f6fb0ea
Avner Eisenberg <avner.e@pepperi.com>
2023-10-12 11:50:33 +0300

    fix state change cpi side

--- client-side/src/app/block/block.component.ts
+++ client-side/src/app/block/block.component.ts
@@ -42,16 +42,6 @@ export class BlockComponent implements OnInit {
         });
     }
 
-    ngAfterViewInit(){
-        // if(this.elementView.nativeElement){
-        //     this.imageMaxHeight = this.elementView.nativeElement.clientHeight || '56';
-        // }
-     }
-    ngOnChanges(e: any): void {
-        // if(this.elementView.nativeElement){
-        //     this.imageMaxHeight = this.elementView.nativeElement.clientHeight || '56';
-        // }
-    }
     private registerStateChange(data: {state: any, configuration: any}) {
         this.configuration = data.configuration;
     }
@@ -66,51 +56,4 @@ export class BlockComponent implements OnInit {
             }
         } 
     }
-  
-    // onBannerClick2(event, bannerID){
-        
-    // if(event?.srcElement?.classList?.value.indexOf(this.configuration.Banners[bannerID].ClickedArea) > -1 || this.configuration.Banners[bannerID].ClickedArea === 'banner'){
-
-    //     const flowData = this.configuration.Banners[bannerID].Flow || null;
-    //     const parameters = {
-    //             configuration: this.configuration
-    //             //: this.configuration.Banners[bannerID]
-    //     }
-    //     if(flowData){
-    //         // Parse the params if exist.
-    //         // const params = this.getScriptParams(event.ScriptData); 
-    //             try{
-    //                 const eventData = {
-    //                     detail: {
-    //                         eventKey: CLIENT_ACTION_ON_BANNER_CLICK,
-    //                         eventData: { flow: flowData, parameters: parameters },
-    //                         completion: (res: any) => {
-    //                                 if (res?.configuration && Object.keys(res.configuration).length > 0) {
-    //                                     this.configuration.Banners[bannerID] = {...this.configuration.Banners[bannerID], ...res.configuration};
-    //                                 } else {
-    //                                     // Show default error.
-    //                                 }
-    //                             }
-    //                     }
-    //                 };
-
-    //                 const customEvent = new CustomEvent('emit-event', eventData);
-    //                 window.dispatchEvent(customEvent);
-    //             }
-    //             catch(err){
-
-    //             }
-    //         }
-    //     }
-    // }
-
-    // onBannerClick(event, bannerID){
-    //     if(event?.srcElement?.classList?.value.indexOf(this.configuration.Banners[bannerID].ClickedArea) > -1 || this.configuration.Banners[bannerID].ClickedArea === 'banner'){
-    //         this.onButtonClick(bannerID);
-    //     }
-    // }
-
-    // onBannerTextClick(event){
-    //     this.onBannerClick(event.event, event.id);
-    // }
 }
--- cpi-side/addon-cpi.ts
+++ cpi-side/addon-cpi.ts
@@ -1,5 +1,6 @@
 import '@pepperi-addons/cpi-node'
 import BannerCpiService from './banner-cpi.service';
+import * as _ from 'lodash'
 
 export async function load(configuration: any): Promise<void>{
     return Promise.resolve();
@@ -24,9 +25,13 @@ router.post('/on_block_load', async (req, res) => {
         configurationRes = result?.configuration || configuration;
     }
 
-    const mergeState = Object.assign(Object.assign({}, state), {configuration: configurationRes});
+    const difference = _.differenceWith(_.toPairs(configurationRes), _.toPairs(configuration), _.isEqual);
+    difference.forEach(diff => {
+        state[diff[0]] = diff[1];
+    });
+
     res.json({
-        State: mergeState,
+        State: state,
         Configuration: configurationRes,
     });
 });
@@ -34,7 +39,16 @@ router.post('/on_block_load', async (req, res) => {
 router.post('/run_button_click_event', async (req, res) => {
     const state = req.body.State;
     const btnID = req.body.ButtonKey;
-    const configuration = state?.configuration || req?.body?.Configuration;
+    const configuration = req?.body?.Configuration;
+
+    for (const prop in configuration) {
+        // skip loop if the property dont exits on state object
+        if (state.hasOwnProperty(prop)) {
+            //update configuration with the object from state
+            configuration[prop] = state[prop];
+        }
+    }
+
     let configurationRes = configuration;
     // check if flow configured to on load --> run flow (instaed of onload event)
     if (configuration?.Banners[btnID]?.Flow){
@@ -43,9 +57,13 @@ router.post('/run_button_click_event', async (req, res) => {
         const result: any = await cpiService.getOptionsFromFlow(configuration.Banners[btnID].Flow || [], state, req.context, configuration);
         configurationRes = result?.configuration || configuration;
     }
-    const mergeState = Object.assign(Object.assign({}, state), {configuration: configurationRes});
+    const difference = _.differenceWith(_.toPairs(configurationRes), _.toPairs(configuration), _.isEqual);
+    difference.forEach(diff => {
+        state[diff[0]] = diff[1];
+    });
+
     res.json({
-        State: mergeState,
+        State: state,
         Configuration: configurationRes,
     });
 });
@@ -53,7 +71,7 @@ router.post('/run_button_click_event', async (req, res) => {
 router.post('/on_block_state_change', async (req, res) => {
     const state = req.body.State || {};
     const changes = req.body.Changes || {};
-    const configuration = req.body.Configuration;
+    //const configuration = req.body.Configuration;
 
     const mergeState = {...state, ...changes};
 
